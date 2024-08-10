2a6160fdba40131239e81800dcf2d2a11d9f08ff
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-10 10:52:26 +0530

    test class comp

--- src/App.jsx
+++ src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Functional from "./components/Functional";
+import ClassComp from "./components/ClassComp";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <>
-      <h1>Class based Components</h1>
+      <ClassComp />
       <Functional />
     </>
   );
