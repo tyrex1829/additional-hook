e29ab016352edc00e0fc7c3cd584b884171ce824
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-10 10:59:36 +0530

    lifecycle of functional component

--- src/components/Functional.jsx
+++ src/components/Functional.jsx
@@ -1,10 +1,25 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 function Functional() {
+  // state of func comp
   const [count, setCount] = useState(0);
+
+  // lifecycle of func state
+  useEffect(() => {
+    // this is data fetching
+    console.log("Component mounted");
+
+    return () => {
+      // this is clean up
+      console.log("component unmounted");
+    };
+  });
 
   return (
     <div>
+      <p>This is for life cycle events</p>
+
+      {/* this is state of functionl comp */}
       <p>{count}</p>
       <button
         onClick={() => {
