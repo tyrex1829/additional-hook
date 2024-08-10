3c396d6d1bfca61d82eb5c97bc0a61643081ce36
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-10 11:25:41 +0530

    lifecycle of class comp

--- src/components/ClassComp.jsx
+++ src/components/ClassComp.jsx
@@ -1,17 +1,33 @@
 import React, { Component } from "react";
 
 export default class ClassComp extends Component {
+  // for state
   constructor(props) {
     super(props);
     this.state = { count: 0 };
   }
-
+  // for state
   incrementCount = () => {
     this.setState({ count: this.state.count + 1 });
   };
 
+  // life cycle
+  componentDidMount() {
+    // data fetching
+    console.log("Component mounted");
+  }
+  componentWillUnmount() {
+    // clean up
+    console.log("Component Unmount");
+  }
+  render() {
+    // UI
+    return <div>life cycle</div>;
+  }
+
   render() {
     return (
+      // for state
       <div>
         <p>{this.state.count}</p>
         <button onClick={this.incrementCount}>Increment</button>
