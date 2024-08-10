08de111ecb16697c59945b1c5c0328c38055a601
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-10 10:51:27 +0530

    code of class comp

---
+++ src/components/ClassComp.jsx
@@ -0,0 +1,21 @@
+import React, { Component } from "react";
+
+export default class ClassComp extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { count: 0 };
+  }
+
+  incrementCount = () => {
+    this.setState({ count: this.state.count + 1 });
+  };
+
+  render() {
+    return (
+      <div>
+        <p>{this.state.count}</p>
+        <button onClick={this.incrementCount}>Increment</button>
+      </div>
+    );
+  }
+}
