7387fd83da21a236018ea7d654314a108de45c70
tyrex1829 <sakshamkhare19@gmail.com>
2024-08-10 10:46:08 +0530

    test functional comp

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,7 @@ import { useState } from "react";
 import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
+import Functional from "./components/Functional";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -9,7 +10,7 @@ function App() {
   return (
     <>
       <h1>Class based Components</h1>
-      <h1>Function based Components</h1>
+      <Functional />
     </>
   );
 }
